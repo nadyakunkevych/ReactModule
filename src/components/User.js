10aad3d8f847e5cbe5b8fd85939d1dff73e1981c
nadiakunkevic <nadyakunkevych@gmail.com>
2022-01-21 01:44:45 +0200

    master base

--- src/components/User.js
+++ src/components/User.js
@@ -5,7 +5,7 @@ const User = ({user, getUser}) => {
     const {id, name} = user;
     return (
         <div className={css.container}>
-            <div className={css.borderBox}> {id}. {name} </div>
+            <div className={css.borderBox}> <strong>{id}.  </strong> {name} </div>
             <button  onClick={()=>getUser(user)}> GetDetails </button>
 
 
