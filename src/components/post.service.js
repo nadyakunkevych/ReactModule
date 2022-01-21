b0b845686b93853d3eefc90ad18c4e0d8bf7498e
nadiakunkevic <nadyakunkevych@gmail.com>
2022-01-21 23:19:48 +0200

    master base

--- src/components/post.service.js
+++ src/components/post.service.js
@@ -2,6 +2,6 @@
 import {urls} from "./urls";
 
 export let postService = {
-    getByUserId: (id) => fetch(`https://jsonplaceholder.typicode.com/${urls.posts}?userId=${id}`).then(value => value.json()),
+    getByUserId: (id) => fetch(`https://jsonplaceholder.typicode.com${urls.posts}?userId=${id}`).then(value => value.json()),
 
 }
