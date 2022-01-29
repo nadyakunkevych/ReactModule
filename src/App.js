32a2cfc60b5ed79b62bc296e348060a9648b1a2f
nadiakunkevic <nadyakunkevych@gmail.com>
2022-01-29 21:03:18 +0200

    master base

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import {Route, Routes} from "react-router-dom"
 
-import './App.css';
+import css from './App.module.css';
+import UsersPage from "./pages/UsersPage/UsersPage"
+import PostsPage from "./pages/PostsPage/PostsPage"
+import SinglePostPage from "./pages/SinglePostPage/SinglePostPage"
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage"
+import Layout from "./components/Layout/Layout";
+import SingleUserPage from "./pages/SingleUserPage/SingleUserPage"
 
-const App = () => {
+function App() {
     return (
-        <div>
+        <>
+            <Routes>
+                <Route path={'/'} element={<Layout/>}>
+                    <Route path={'users'} element={<UsersPage/>}>
+                        <Route path={':id'} element={<SingleUserPage/>}/>
+                    </Route>
+                    <Route path={'posts'} element={<PostsPage/>}>
+                        <Route path={':id'} element={<SinglePostPage/>}/>
+                    </Route>
+                    <Route path={'*'} element={<NotFoundPage/>}/>
+                </Route>
 
-        </div>
+            </Routes>
+        </>
     );
-};
+}
 
 export default App;
