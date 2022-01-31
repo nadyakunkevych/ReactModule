a258befed7d2f4c4665508199e33d9903e43f250
nadiakunkevic <nadyakunkevych@gmail.com>
2022-01-31 22:45:56 +0200

    master base

---
+++ src/components/Test/Test.js
@@ -0,0 +1,12 @@
+import {Component} from "react";
+
+
+class Test extends Component {
+        render () {
+              return (
+                 <div> Hello from class components </div>
+        )
+    }
+}
+
+export {Test}
