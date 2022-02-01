694dfb33e38b323d346cde4f667c31daca8fb2a8
nadiakunkevic <nadyakunkevych@gmail.com>
2022-02-01 21:33:35 +0200

    master base

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import React from 'react';
-import {Test} from "./components/Test/Test";
+import React, {useReducer} from 'react';
+import css from "./components/Test/Test.module.css";
+
+const reducer = (state, action) => {
+    switch(action.type) {
+        case 'inc':
+            return {...state, count1: state.count1 +1}
+        case 'dec':
+            return {...state, count1: state.count1 -1}
+        case 'reset':
+            return {...state, count1: 0}
+    }
+    return state
+
+}
 
 const App = () => {
+   const [state, dispatch] = useReducer(reducer, {count1: 0});
+
+
     return (
-        <>
-            <Test/>
-        </>
+        <div className={css.cover}>
+            <div className={css.mainWrap}>
+            <h2 className={css.number}> Number: {state.count1} </h2>
+            <div className={css.buttonWrap}>
+          <div className={css.btn}>
+            <button onClick={()=>dispatch({type: 'inc'})} >increase </button> </div>
+                <div className={css.btn}>
+                    <button onClick={()=>dispatch({type: 'dec'})}> decrease </button></div>
+                <div className={css.btn}>
+                    <button onClick={()=>dispatch({type: 'reset'})}> reset </button></div>
+          </div>
+
+            </div>
+        </div>
     );
 };
 
